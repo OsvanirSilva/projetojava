0c0bed7cb06626a5a797f2146a4351e20584b1d1
Rafael <bonani_rafa_12@outlook.com>
2026-05-05 12:07:04 -0300

    - Colocado POST para listar Produto BD via Postman. - Porta 8080 forcada.

--- src/main/java/com/projeto/java/controller/LanchoneteController.java
+++ src/main/java/com/projeto/java/controller/LanchoneteController.java
@@ -2,9 +2,8 @@ package com.projeto.java.controller;
 import com.projeto.java.model.Produto;
 import com.projeto.java.model.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -19,5 +18,9 @@ public class LanchoneteController {
         return produtoRepository.findAll(); // O JPA busca tudo no banco e converte em JSON
     }
 
+    @PostMapping
+    public List<Produto> addProduto(@RequestBody List<Produto> novoProduto){
+        return produtoRepository.saveAll(novoProduto); //JSON com [] obrigatório (lista)
+    }
 
 }
